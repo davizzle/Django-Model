03cb7de7ad6db48197531fd38beedd77579c4df6
davizzle <victorgodwinhet@gmail.com>
2022-06-18 23:42:48 +0100

    created blog app and new model

---
+++ I4G008164QIZ/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import DateTimeField
+
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
